f745fdc948953d342cccf87cffca385048ad3756
Romeo Cosma <122970841+RoMeRO93@users.noreply.github.com>
2023-04-10 14:53:07 +0300

    Add project files.

---
+++ array test1/Program.cs
@@ -0,0 +1,27 @@
+﻿internal class Program
+{
+    private static void Main(string[] args)
+    {
+        int i;
+
+        string[] cars = { "Dacia", "Aro", "DAC", "Marta" };
+
+        int[] myNum = { 1310, 240, 665, 415 };
+        for (i = 0; i < 1; i++)
+        {
+            Console.Write(cars[0]);
+            Console.Write("  ");
+            Console.WriteLine(myNum[0]);
+            Console.Write(cars[1]);
+            Console.Write("  ");
+            Console.WriteLine(myNum[1]);
+            Console.Write(cars[2]);
+            Console.Write("  ");
+            Console.WriteLine(myNum[2]);
+            Console.Write(cars[3]);
+            Console.Write("  ");
+            Console.WriteLine(myNum[3]);
+        }
+
+    }
+}
